630b10fc152f653843fb290b2ee589113dacf1df
esqvcrpt <jairoguedess@hotmail.com>
2026-01-10 19:34:04 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
 const withdrawalsRoutes = require("./routes/withdrawals");
 
@@ -9,18 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// rota raiz
 app.get("/", (req, res) => {
-  res.json({ status: "Esquiva Gateway backend online" });
+  res.send("Esquiva Backend rodando 🚀");
 });
 
-// rotas admin
-app.use("/admin", withdrawalsRoutes);
-
-// porta
-const PORT = process.env.PORT || 3000;
-import withdrawalsRoutes from "./routes/withdrawals.js";
 app.use("/withdrawals", withdrawalsRoutes);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
