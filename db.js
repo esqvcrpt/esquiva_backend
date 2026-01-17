0e4208509c9a1524ca60547270ba6eb0eb2e0254
esqvcrpt <jairoguedess@hotmail.com>
2026-01-17 15:52:31 -0300

    Atualizar o db.js

--- db.js
+++ db.js
@@ -3,9 +3,7 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
