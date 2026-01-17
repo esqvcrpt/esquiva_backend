630a443a01c74eb73017f18f1fdabb989f8926d5
esqvcrpt <jairoguedess@hotmail.com>
2026-01-17 16:16:04 -0300

    Atualizar o db.js

--- db.js
+++ db.js
@@ -2,8 +2,10 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.postgresql://esquiva:XkMLAa9XpLvHAvY3pJdb1nZ2dqt7FUQW@dpg-d5hda16r433s73bmlh5g-a/esquiva,
-  ssl: { rejectUnauthorized: false },
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
